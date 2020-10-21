8a3f8d15fc24f1dd4a0d3cbbdd1a3eb444655f6c
jianmingtu <tu_jm@yahoo.com>
2020-10-21 15:47:40 -0700

    Create the javascript code to allow users to view their total (before, after taxes), add/remove items & increase quantity of items #21

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,15 @@
-function createUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (
-        c
-    ) {
-        var r = (Math.random() * 16) | 0,
-            v = c == 'x' ? r : (r & 0x3) | 0x8;
-        return v.toString(16);
-    });
-}
+let quantityInputsDOM = [];
+let cart = [{ "id": "1", "quantity": "1" },
+{ "id": "2", "quantity": "1" },
+{ "id": "3", "quantity": "1" },
+{ "id": "4", "quantity": "1" },
+{ "id": "5", "quantity": "1" },
+{ "id": "6", "quantity": "1" },
+{ "id": "7", "quantity": "1" },
+{ "id": "8", "quantity": "1" },
+{ "id": "9", "quantity": "1" },
+{ "id": "10", "quantity": "1" },
+];
 
 const RuturnCode = {
     //assinging values to constants
@@ -14,195 +17,137 @@ const RuturnCode = {
     SUCCESS: 'Function return success',
 };
 
-const CustomersFile = './customers.json';
-
-class Wine {
-    constructor(name, picture, brand, price, descrption, quantity) {
-        this.name = name;
-        this.picture = picture;
-        this.brand = brand;
-        this.price = price;
-        this.descrption = descrption;
-        this.quantity = quantity;
-    }
-}
-
-class Wines {
-    constructor() {
-        this.wines = new Map([]);
-    }
-
-    // add(wine) {
-    //     this.wines.push(wine);
-    // }
-}
-
-class CustomerInventory extends Wines {
-    constructor() {
-        super();
-    }
-
-    // the customer wine only contains wine name and quantity
-    addWine(name, quantity) {
-        // add a number of wine into the database: check the quantities against the inventory
-        var ret = RuturnCode.ERROR;
-        var quantities = gInventory.get(name, quantity);
-        if (typeof quantities !== 'undefined' && quantity < quantities) {
-            var customerQuantities = this.wines.get(name);
-            if (typeof customerQuantities !== 'undefined') {
-                this.wines.set(name, quantities + quantity);
-            }
-        }
-        return ret;
-    }
-}
-
-class Inventory extends Wines {
-    constructor() {
-        super();
-        this.wines.set(
-            'Russia Wine 1',
-            new Wine(
-                'Russia Wine 1',
-                '/images/russia/1.png',
-                22.22,
-                'Drunk Russia Man',
-                100
-            )
-        );
-        this.wines.set(
-            'China Wine 1',
-            new Wine(
-                'China Wine 1',
-                '/images/china/1.png',
-                22.22,
-                'Drunk China Man',
-                100
-            )
-        );
-    }
-
-    getQuantities(name) {
-        var quantities = 0;
-        quantiies = this.wines.get(name);
-        return quantities;
-    }
-}
-
-class Customer {
-    constructor(name, birthday, email, phone, address, password) {
-        this.name = name;
-        this.birthday = birthday;
-        this.email = email;
-        this.phone = phone;
-        this.address = password;
-        this.wines = new Map(); // wine list but the quantity is the purchased wine quantities
-    }
-
-    addWine(name, quantity) {
-        return addWine(name, quantity);
-    }
-
-    removeWine(wineName, quantity) {}
-}
-
 class Customers {
-    constructor() {
-        this.customers = new Map();
+    // get Product from product.json file
+    async getCustomers() {
+
+        try {
+            let result = await fetch('customers.json');
+            let data = await result.json();
+            this.customers = data.map(customer => {
+                return customer;
+            });
+
+            return this.customers;
+        } catch (error) {
+            console.log(error);
+
+        }
     }
 
-    add(customer) {
-        this.customers.set(customer.name, customer);
+    // set Products to localStorage
+    static saveLoaclCustomers(customers) {
+        localStorage.setItem("customers", JSON.stringify(customers));
+        localStorage.setItem("cart", JSON.stringify(customers));
     }
 
-    saveJSONFile() {
-        // convert Map to JSON string
-        // const jsonStr = JSON.stringify(Array.from(gCustomers.customers.entries()));
-        // // save JSON to file
-        // const fs = require("fs");
-        // fs.writeFile(CustomersFile, jsonStr, err => { console.log("Error happens while saving customers.json") })
+    // get Products from localStorage
+    static getLoaclCustomers() {
+        var carts = [...localStorage.getItem("cart")]
     }
 
-    readJSONFile() {
-        // // Read JSON file
-        // const fs = require("fs");
-        // fs.readFile(CustomersFile, (err, data) => {
-        //     if (err) throw err;
-        //     // convert JSON string to Map
-        //     var obj = JSON.parse(jsonStr, (key, value) => {
-        //         return value;
-        //     });
-        // });
+    //display Products in the browser
+    static displayCustomers(customers) {
+
     }
 }
 
-let gCustomers = new Customers();
-let gInventory = new Inventory();
+class Products {
 
-function test() {
-    gCustomers.add(new Customer('Karen Mok', '1900', '', '', '', ''));
-    gCustomers.saveJSONFile();
+    // get Product from product.json file
+    async getProducts() {
+
+        try {
+            let result = await fetch('products.json');
+            let data = await result.json();
+            this.products = data.map(product => {
+                return product;
+            });
+
+            return this.products;
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
+    // set Products to localStorage
+    static saveLoaclProducts(products) {
+        localStorage.setItem("products", JSON.stringify(products));
+        localStorage.setItem("cart", JSON.stringify(products));
+    }
+
+    // get Products from localStorage
+    static getLocalProducts() {
+        var carts = [...localStorage.getItem("cart")]
+    }
+
+    //display Products in the browser
+    static displayProducts(products) {
+
+        let result = '';
+        products.forEach(product => {
+            result += `
+                <div>
+                    <img src=${product.image} alt="product" class="product-img">
+                    <input class="quantity-box"  data-name=${product.name} data-id=${product.id} type="number"  value="1" />
+                    <h3>${product.type}</h3>
+                    <h4>${product.price}</h4>
+                    <h4>${product.description}</h4>
+                </div>         
+            `;
+
+            var outputDOM = document.querySelector('#output');
+            outputDOM.innerHTML = result;
+        });
+    }
+
+    static getProduct(id) {
+        let products = JSON.parse(localStorage.getItem(products));
+        return products.find(product => product.id === id);
+    }
+
+    static getCarts() {
+
+        // const quantityInputs = [...document.querySelectorAll('.quantity')];
+        // quantityInputsDOM = quantityInputs;
+        // quantityInputs.forEach(input => {
+        //     button.addEventListener('click', (event => {
+        //         event.target.innerText = "In Cart";
+        //     }));
+        // }
+        // })
+    }
 }
 
-test();
+const products = new Products();
+products.getProducts().then(products => {
+    Products.saveLoaclProducts(products);
+    Products.displayProducts(products);
+}).then(() => {
 
-var wineData = [
-    {
-        id: '1',
-        image: '/img/shop-img.png',
-        name: 'TestWine1',
-        price: '$100.00',
-    },
-    {
-        id: '2',
-        image: '/img/shop-img.png',
-        name: 'TestWine2',
-        price: '$30.00',
-    },
-    {
-        id: '3',
-        image: '/img/shop-img.png',
-        name: 'TestWine3',
-        price: '$10.00',
-    },
-    {
-        id: '4',
-        image: '/img/shop-img.png',
-        name: 'TestWine4',
-        price: '$100.00',
-    },
-    {
-        id: '5',
-        image: '/img/shop-img.png',
-        name: 'TestWine5',
-        price: '$30.00',
-    },
-    {
-        id: '6',
-        image: '/img/shop-img.png',
-        name: 'TestWine6',
-        price: '$10.00',
-    },
-];
+});
 
-displayWine();
+document.addEventListener("DOMContentLoaded", () => {
 
-function displayWine() {
-    var out = '';
-    wineData.forEach((wine) => {
-        out += '<div>';
-        out += "<img src='" + wine.image + "'>";
-        out += '<h2>' + wine.name + '</h2>';
-        out += '<p>' + wine.price + '</p>';
-        out +=
-            "<input type='number' id='wine" +
-            wine.id +
-            "' name='wine" +
-            wine.id +
-            "' min='1' max='100'>";
-        out += "<button id='wineButton" + wine.id + "'>Add</button>";
-        out += '</div>';
-        document.querySelector('#wineDisplay').innerHTML += out;
-        out = '';
-    });
-}
+    var url = window.location.href;
+    if (url.search("index.html")) {
+
+        const products = new Products();
+        const customers = new Customers();
+        products.getProducts().then(products => {
+            Products.saveLoaclProducts(products);
+            Products.displayProducts(products);
+        }).then(() => {
+            let values = 0;
+            var submitBtn = document.querySelector('.proceed-btn');
+            submitBtn.addEventListener('click', event => {
+                const quantityInputs = [...document.querySelectorAll('.quantity-box')];
+                quantityInputs.map(input => {
+                    values += parseFloat(input.value);
+                })
+            });
+        })
+
+    }
+});
