b91dab6f7946904661ff1e8c36e40f4e1dbf2d5d
Wayne Lee <waynel1996weiyang@gmail.com>
2020-10-21 18:36:09 -0700

    Load data from json, display data from json, add click listener to add button to store to localStorage

--- scripts/index.js
+++ scripts/index.js
@@ -146,63 +146,43 @@ function test() {
 
 test();
 
-var wineData = [
-    {
-        id: '1',
-        image: '/img/shop-img.png',
-        name: 'TestWine1',
-        price: '$100.00',
-    },
-    {
-        id: '2',
-        image: '/img/shop-img.png',
-        name: 'TestWine2',
-        price: '$30.00',
-    },
-    {
-        id: '3',
-        image: '/img/shop-img.png',
-        name: 'TestWine3',
-        price: '$10.00',
-    },
-    {
-        id: '4',
-        image: '/img/shop-img.png',
-        name: 'TestWine4',
-        price: '$100.00',
-    },
-    {
-        id: '5',
-        image: '/img/shop-img.png',
-        name: 'TestWine5',
-        price: '$30.00',
-    },
-    {
-        id: '6',
-        image: '/img/shop-img.png',
-        name: 'TestWine6',
-        price: '$10.00',
-    },
-];
+// var wineData = [
+//     {
+//         id: '1',
+//         image: '/img/shop-img.png',
+//         name: 'TestWine1',
+//         price: '$100.00',
+//     },
+//     {
+//         id: '2',
+//         image: '/img/shop-img.png',
+//         name: 'TestWine2',
+//         price: '$30.00',
+//     },
+//     {
+//         id: '3',
+//         image: '/img/shop-img.png',
+//         name: 'TestWine3',
+//         price: '$10.00',
+//     },
+//     {
+//         id: '4',
+//         image: '/img/shop-img.png',
+//         name: 'TestWine4',
+//         price: '$100.00',
+//     },
+//     {
+//         id: '5',
+//         image: '/img/shop-img.png',
+//         name: 'TestWine5',
+//         price: '$30.00',
+//     },
+//     {
+//         id: '6',
+//         image: '/img/shop-img.png',
+//         name: 'TestWine6',
+//         price: '$10.00',
+//     },
+// ];
 
-displayWine();
 
-function displayWine() {
-    var out = '';
-    wineData.forEach((wine) => {
-        out += '<div>';
-        out += "<img src='" + wine.image + "'>";
-        out += '<h2>' + wine.name + '</h2>';
-        out += '<p>' + wine.price + '</p>';
-        out +=
-            "<input type='number' id='wine" +
-            wine.id +
-            "' name='wine" +
-            wine.id +
-            "' min='1' max='100'>";
-        out += "<button id='wineButton" + wine.id + "'>Add</button>";
-        out += '</div>';
-        document.querySelector('#wineDisplay').innerHTML += out;
-        out = '';
-    });
-}
---
+++ scripts/shop.js
@@ -0,0 +1,73 @@
+//On page load
+document.addEventListener("DOMContentLoaded", () => {
+    showProducts();
+});
+
+function showProducts() {
+    const products = new Products();
+    //Get from json before doing anything
+    products.getProducts().then(data => {        
+        displayWine(data);
+    });
+}
+
+//Fetch data from json
+class Products {
+    async getProducts() {
+        try {
+            let result = await fetch('scripts/products.json');
+            let data = await result.json();
+            this.products = data.map(product => {
+                return product;
+            });
+            return this.products;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+function displayWine(wineData) {
+    //For displaying the wine on page
+    var out = '';
+    document.querySelector('#wineDisplay').innerHTML = '';
+    wineData.forEach((wine) => {
+        out += '<div>';
+        out += "<img src='" + wine.image + "'>";
+        out += '<h2>' + wine.name + '</h2>';
+        out += '<p>' + wine.price + '</p>';
+        out +=
+            "<input type='number' id='wine" +
+            wine.id +
+            "' name='wine" +
+            wine.id +
+            "' min='1' max='100' value='1'>";
+        out += "<button id='wineButton" + wine.id + "' class='addButton'>Add</button>";
+        out += '</div>';
+        document.querySelector('#wineDisplay').innerHTML += out;
+        out = '';
+    });
+
+    //Add click listener to button
+    const addBtns = document.querySelectorAll('.addButton');
+    addBtns.forEach(btn => {
+        var btnId = btn.id.match(/(\d+)/)[0];
+        var input = document.querySelector("#wine" + btnId);
+        btn.addEventListener("click", function() {
+            var quantity;
+            var storeObj;
+            if (localStorage.getItem(btn.id) == null) {
+                quantity = input.value;
+            } else {
+                quantity = JSON.parse(localStorage.getItem(btn.id)).amount + parseInt(input.value);
+            }
+            storeObj = {id: btnId, amount: quantity};
+            localStorage.setItem(btn.id, JSON.stringify(storeObj));
+        });
+    });
+}
+
+var orderFilter = document.querySelectorAll("[name=listOrder]");
+orderFilter.forEach(order => order.addEventListener("change", function() {
+    showProducts();
+}));
