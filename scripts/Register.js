96b59bcc7d7a551693d2435de911fa0876944b02
Kalvin Tang <ktang82@my.bcit.ca>
2020-10-24 16:21:03 -0700

    form input spacing #30

--- scripts/Register.js
+++
@@ -1,8 +0,0 @@
-function validate() {
-    var x = document.forms["Regform"]["firstName"].value;
-
-    if (x == " " || x == null) {
-        alert("First name must be filled out!");
-        return false;
-    }
-}
