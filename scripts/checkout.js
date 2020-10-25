fd8b99ff279c676c12e6a6996449a4ccfcc90271
jianmingtu <tu_jm@yahoo.com>
2020-10-24 18:20:15 -0700

    Parse Json objects #12 - turn on prettier for vscode and save these four files again using prettier.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,28 +1,28 @@
 let products = [];
 
 class Cart {
-    // get cart from localStorage
-    static getLocal() {
-        var cart = [];
-        for (var i = 0; i < localStorage.length; i++) {
-            var key = localStorage.key(i);
-            var value = localStorage.getItem(key);
-            cart.push({ "id": key, "amount": value });
-        }
-
-        return cart;
+  // get cart from localStorage
+  static getLocal() {
+    var cart = [];
+    for (var i = 0; i < localStorage.length; i++) {
+      var key = localStorage.key(i);
+      var value = localStorage.getItem(key);
+      cart.push({ id: key, amount: value });
     }
 
-    static showCart(cart) {
-        //display Cart in the browser
-        let result = '';
-        let total = 0;
-        cart.forEach(cartItem => {
-            let product = products.find(product => product.id === cartItem.id);
-            let subTotal = parseFloat(cartItem.amount) * parseFloat(product.price);
-            total += subTotal;
+    return cart;
+  }
 
-            result += `
+  static showCart(cart) {
+    //display Cart in the browser
+    let result = "";
+    let total = 0;
+    cart.forEach(cartItem => {
+      let product = products.find(product => product.id === cartItem.id);
+      let subTotal = parseFloat(cartItem.amount) * parseFloat(product.price);
+      total += subTotal;
+
+      result += `
                     <div class="container">
                     <div class="d-flex my-4 flex-column flex-md-row align-items-center justify-content-around ">
                         <!-- product 1 img -->
@@ -38,22 +38,25 @@ class Cart {
                         <!-- product 1 quantity and unit price -->
                         <div class="product-item">
                             <div class="d-flex flex-row align-items-center justify-content-center">
-                                <p class="price mx-4">$${product.price} x ${cartItem.amount}</p>
+                                <p class="price mx-4">$${product.price} x ${
+        cartItem.amount
+      }</p>
 
                             </div>
                         </div>
                         <!-- product 1 subtotal -->
                         <div class="product-item">
-                            <p class="price my-5 mx-3 item-total">$${subTotal.toFixed(2)}</p>
+                            <p class="price my-5 mx-3 item-total">$${subTotal.toFixed(
+                              2
+                            )}</p>
                         </div>  
                     </div>  
                 </div>      
                 `;
-        });
+    });
 
-
-        result += `<div class="line"></div>`;
-        result += `
+    result += `<div class="line"></div>`;
+    result += `
 
             <div class="container">
             <div class="d-flex my-4 flex-column flex-md-row align-items-center justify-content-around ">
@@ -71,46 +74,41 @@ class Cart {
                 </div>
                 <!-- product 1 subtotal -->
                 <div class="product-item">
-                    <p class="price my-5 mx-3 item-total">$${total.toFixed(2)}</p>
+                    <p class="price my-5 mx-3 item-total">$${total.toFixed(
+                      2
+                    )}</p>
                 </div>  
             </div>  
         </div>            
         `;
 
-        var outputDOM = document.querySelector('.cart-checkout');
-        outputDOM.innerHTML = result;
-    }
+    var outputDOM = document.querySelector(".cart-checkout");
+    outputDOM.innerHTML = result;
+  }
 }
 
 class Products {
-
-    // get Product from product.json file
-    async get() {
-
-        try {
-            var loc = window.location.pathname;
-            let result = await fetch('./scripts/products.json');
-            return await result.json();
-        } catch (error) {
-            console.log(error);
-        }
+  // get Product from product.json file
+  async get() {
+    try {
+      var loc = window.location.pathname;
+      let result = await fetch("./scripts/products.json");
+      return await result.json();
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  new Products().get().then(tempProducts => {
+    // get products from products.json file and save it to the global variable products
+    products = tempProducts;
 
-    (new Products()).get().then(
+    // display shopping cart
+    Cart.showCart(Cart.getLocal());
 
-        tempProducts => {
-
-            // get products from products.json file and save it to the global variable products
-            products = tempProducts;
-
-            // display shopping cart
-            Cart.showCart(Cart.getLocal());
-
-            //clear local storage on submitting the form
-            addSubmitListener();
-        })
+    //clear local storage on submitting the form
+    addSubmitListener();
+  });
 });
-
