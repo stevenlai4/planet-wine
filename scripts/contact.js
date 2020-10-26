44472d246ddda111a9d844a29b9771279853c924
Karen <karen@Karens-MBP.hitronhub.home>
2020-10-26 15:53:14 -0700

    addded validation for name

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,26 +2,26 @@ const form = document.getElementById('form');
 const name = document.getElementById('name');
 const email = document.getElementById('email');
 const question = document.getElementById('question');
-
+var alphaOnly = /^[a-zA-Z]*$/;
 form.addEventListener('submit', e => {
-	e.preventDefault();
-	checkInputs();
+    e.preventDefault();
+    checkInputs();
 });
-
 function checkInputs(){
     // get the values from the inputs
     const nameValue = name.value.trim();
     const emailValue = email.value.trim();
     const questionValue = question.value.trim();
-
     if(nameValue === ''){
         //show error and add error class
         setErrorFor(name,"Name cannot be blank");
-    } else {
-        //add success class
+    } else  if (nameValue.match(alphaOnly)){
+        //only accept alpha , add success class
         setSuccessFor(name);
+    } else {
+        //add error class
+        setErrorFor(name,'Name is not valid, only alphabetical characters')
     }
-
     if(emailValue === ''){
         //show error and add error class
         setErrorFor(email,"Email cannot be blank");
@@ -31,7 +31,6 @@ function checkInputs(){
         //add success class
         setSuccessFor(email);
     }
-
     if(questionValue === ''){
         //show error and add error class
         setErrorFor(question,"Please leave your questions");
@@ -41,8 +40,12 @@ function checkInputs(){
         //add success class
         setSuccessFor(question);
     }
+    if (name.parentElement.className == 'contactForm success' && email.parentElement.className == 'contactForm success' && question.parentElement.className == 'contactForm success'){
+         // pop-up after successful submittion/reset form
+        swal("Thank you for getting in touch!", "We will get back in touch with you soon.");
+        document.getElementById("form").reset();
+    }
 }  
-
 function setErrorFor(input,message){
     const contactForm = input.parentElement;
     const small = contactForm.querySelector('small');
@@ -51,14 +54,12 @@ function setErrorFor(input,message){
     //add error class
     contactForm.className = 'contactForm error';
 }
-
 function setSuccessFor(input) {
     const contactForm = input.parentElement;
     //add success class
     contactForm.className = 'contactForm success';
 }
-
 //make sure the email is valid
 function isEmail(email) {
-	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
