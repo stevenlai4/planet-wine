80cc02ecad050d9d7bf10e0d0a4a1d8ef8a442bb
Steven <clai86@my.bcit.ca>
2020-10-26 20:07:29 -0700

    Register page finished

--- scripts/Register.js
+++ scripts/Register.js
@@ -1,22 +1,22 @@
 var alphaOnly = /^[a-zA-Z]*$/;
-document.addEventListener("submit", (e) => {
+document.addEventListener('submit', (e) => {
     e.preventDefault();
     if (validation(this)) {
         swal(
-            "Thank you for your registration!",
-            "You will receive an email confirmation shortly."
+            'Thank you for your registration!',
+            'You will receive an email confirmation shortly.'
         );
     }
 });
 
 const validationFields = [
-    "firstName",
-    "LastName",
-    "email",
-    "address",
-    "postalCode",
-    "password",
-    "conPassword",
+    'firstName',
+    'LastName',
+    'email',
+    'address',
+    'postalCode',
+    'password',
+    'conPassword',
 ];
 
 //clear local storage on submitting the form and validation
@@ -53,46 +53,49 @@ function validation(form) {
     }
 
     let isError = false;
-    let password = "";
+    let password = '';
 
     validationFields.forEach((field) => {
         const isRequired =
             [
-                "firstName",
-                "LastName",
-                "email",
-                "address",
-                "postalCode",
-                "password",
-                "conPassword",
+                'firstName',
+                'LastName',
+                'email',
+                'address',
+                'postalCode',
+                'password',
+                'conPassword',
             ].indexOf(field) != -1;
 
         if (isRequired) {
-            const item = document.querySelector("#" + field);
+            const item = document.querySelector('#' + field);
 
             if (item) {
                 const value = item.value.trim();
-                if (value === "") {
-                    setErrorFor(item, field + " cannot be blank");
+                if (value === '') {
+                    setErrorFor(item, field + ' cannot be blank!');
                     isError = true;
-                } else if (field === "email" && !isEmail(value)) {
-                    setErrorFor(item, field + " is an invalid email");
+                } else if (field === 'email' && !isEmail(value)) {
+                    setErrorFor(item, 'Invalid Email Address!');
                     isError = true;
-                } else if (field === "postalCode" && !isPostCode(value)) {
-                    setErrorFor(item, field + " is an invalid post code");
+                } else if (field === 'postalCode' && !isPostCode(value)) {
+                    setErrorFor(item, 'Invalid Postal Code!');
                     isError = true;
-                } else if (field === "phone" && !isPhone(value)) {
-                    setErrorFor(item, field + " is an invalid phone number");
+                } else if (field === 'phone' && !isPhone(value)) {
+                    setErrorFor(item, 'Invalid Phone Number!');
                     isError = true;
-                } else if (field === "password" && isPassword(value)) {
+                } else if (field === 'password' && isPassword(value)) {
                     setSuccessFor(item);
                     password = value;
-                } else if (field === "password" && !isPassword(value)) {
-                    setErrorFor(item, field + " is an invalid password");
+                } else if (field === 'password' && !isPassword(value)) {
+                    setErrorFor(
+                        item,
+                        ' Minimum 7 and Maximum 15 characters, numeric digits, underscore and first character must be a letter!'
+                    );
                     isError = true;
-                    password = "";
-                } else if (field === "conPassword" && password !== value) {
-                    setErrorFor(item, field + " is an invalid password");
+                    password = '';
+                } else if (field === 'conPassword' && password !== value) {
+                    setErrorFor(item, 'Confirmation Password Not Match!');
                     isError = true;
                 } else {
                     setSuccessFor(item);
@@ -106,18 +109,18 @@ function validation(form) {
 
 function setErrorFor(input, message) {
     const formgroup = input.parentElement;
-    const small = formgroup.querySelector("small");
+    const small = formgroup.querySelector('small');
     //add error message
     small.innerText = message;
     //add error class
-    formgroup.classList.remove("success");
-    formgroup.classList.add("error");
+    formgroup.classList.remove('success');
+    formgroup.classList.add('error');
 }
 function setSuccessFor(input) {
     const formgroup = input.parentElement;
-    const small = formgroup.querySelector("small");
+    const small = formgroup.querySelector('small');
     //add success class
-    small.innerText = "";
-    formgroup.classList.remove("error");
-    formgroup.classList.add("success");
+    small.innerText = '';
+    formgroup.classList.remove('error');
+    formgroup.classList.add('success');
 }
